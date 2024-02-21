f7b3487d765bc4954251ac5bcbc3f365990ca18d
hamzwee <160797324+hamzwee@users.noreply.github.com>
2024-02-21 15:46:05 -0800

    Add files via upload

---
+++ javascript task/app.js
@@ -0,0 +1,236 @@
+// Chapter #01 ALERT
+
+// Q1
+// alert("HELLO WORLD")
+
+// Q2
+// alert("Error ! Please insert a valid password")
+
+// Q3
+// alert("Welcome to js Land...\nHappy Coding")
+
+// Q4
+// alert("Welcome to JS land...")
+// alert("Happy Coding!")
+
+// Q5
+console.log("Hello I can run JS through my web browser's console")
+
+
+// Q6
+// alert("HELLO WELCOME to my landing page!..")
+
+// Q7
+// ..........
+
+
+
+
+// CHAP#02 VARIABLE FOR STRING
+// Q1
+// var username;
+// let username;
+
+// Q2
+// var myName = "John";
+// let myName = "Your Full Name";
+
+// Q3
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// Q4
+// var studentName = "John";
+// var studentAge = 20;
+// var studentCertificate = "Certified Mobile Application Development";
+
+// alert("Student Name: " + studentName);
+// alert("Student Age: " + studentAge);
+// alert("Student Certificate: " + studentCertificate);
+
+// Q5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// Q6
+// var email = "aqhamza81@gmail.com";
+// var message = "My email address is: " + email;
+// alert(message);
+
+
+// // Q7
+// // Declare a variable called book and assign it the value "A smarter way to learn JavaScript"
+// var book = "A smarter way to learn JavaScript";
+
+// // Create a message using string concatenation, where we replace 'book' with the value of the book variable
+// var message = "I am trying to learn from the Book\n"+book + " ";
+
+// // Display the message in an alert box
+// alert(message);
+
+
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+// Q9
+// var specialString = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+// alert(specialString);
+
+// document.write(specialString);
+
+
+
+// CHAP#03 VARIABLE FOR NUMBERS
+// Q1
+// var age = 15;
+// alert("I am " + age + " years old.");
+
+
+// Q2  
+// var numberOfVisits = localStorage.getItem('visits');
+
+// if (!numberOfVisits) {
+//     numberOfVisits =13;
+// }
+
+// numberOfVisits++;
+
+// localStorage.setItem('visits', numberOfVisits);
+
+// alert("You have visited this site " + numberOfVisits + " times.");
+
+
+// // Q3
+// var birthYear =  2000; 
+// document.write("My birth year is \n" + birthYear + "."+"\nData type of my declared Variable is number");
+
+
+// Q4
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
+
+// ................................
+
+
+// CHAP01 ALERTS
+// Q1
+// var firstName = "John";
+// var lastName = "Doe";
+// var email = "johndoe@example.com";
+// var phoneNumber = "123-456-7890";
+// var password = "secretPassword";
+
+// alert("First Name: " + firstName);
+// alert("Last Name: " + lastName);
+// alert("Email: " + email);
+// alert("Phone Number: " + phoneNumber);
+// alert("Password: " + password);
+
+
+// // Q2
+// alert("You're learning JavaScript!");
+
+
+// Q3
+// alert("Hello there! Welcome to the world of JavaScript!");
+
+
+
+// CHAP02 Variables for string
+// Q1
+// var myVariableName = "value";
+
+
+// Q2
+// var myVariable;
+// myVariable = "This is a string of my choice";
+
+
+// Q3
+// var teamName = "JAWAN PAKISTAN";
+// alert(teamName);
+
+
+// Q4
+// var bestMan = "Charlie";
+// bestMan = "David";
+
+
+
+// CHAP03 Variables for numbers
+// Q1
+// var caseQty;
+
+
+// // Q2
+// var caseQty; // Declaring the variable caseQty
+// caseQty = 144; // Assigning the value 144 to the variable caseQty
+
+
+// Q3
+// var num = 9;
+
+
+// // Q4
+// var sum; // Declare a variable named sum
+// sum = 5 + 3; // Assign it the sum of 5 and 3
+
+
+// Q5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// console.log(orderTotal);
+
+
+// Q6
+// var myNumber = 5; 
+// myNumber = myNumber + 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
